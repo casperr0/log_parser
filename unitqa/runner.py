2ee305a0764067e917c0a418b9d3065e09e14765
thecspr <v.suisu@gmail.com>
2019-09-10 23:32:49 +0300

    fix: save info from captures in the dictionary

--- unitqa/runner.py
+++ unitqa/runner.py
@@ -1,6 +1,4 @@
-
 import json
-import time
 from datetime import datetime
 from datetime import timezone
 
@@ -9,6 +7,7 @@ def start():
     class Test_Suite:
         log_in = {}
         suites_in = {}
+
         def __init__(self, json1, json2):
             for (json_logs, json_suites) in zip(json1['logs'], json2['suites']):
                 all_in = {}
@@ -36,10 +35,7 @@ def start():
             print(self.suites_in)
 
     class Test_Capture:
-        expected = []
-        actual = []
-        time = []
-        list_in = []
+        capture_in = {}
 
         def __init__(self, json):
             for i in json['captures']:
@@ -47,27 +43,26 @@ def start():
                 all_in = {}
                 all_in['expected'] = i['expected']
                 all_in['actual'] = i['actual']
-                self.list_in.append(all_in)
-                self.expected.append(i['expected'])
-                self.actual.append(i['actual'])
-                self.time.append(i['time'])
+                DATE_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
+                t = datetime.strptime(i['time'], DATE_FORMAT)
+                t = int(t.replace(tzinfo=timezone.utc).timestamp())
+                self.capture_in[t] = all_in
 
         def print(self):
-            print(self.expected, self.actual, self.time)
+            print(self.capture_in)
 
     # Read JSON data into the datastore variable
     with open('logs', 'r') as f:
         datastore_logs = json.load(f)
+        print("------")
         print(datastore_logs)
-        # logs = Test_Log(datastore_logs)
-        # logs.print()
-        print("----------")
 
     with open('suites', 'r') as f1:
         with open('logs', 'r') as f2:
             datastore_suites = json.load(f1)
             datastore_logs = json.load(f2)
             suites = Test_Suite(datastore_logs, datastore_suites)
+            print("------")
             suites.print()
 
         # datastore_suites = json.load(f)
@@ -82,43 +77,19 @@ def start():
         captures.print()
         f.close()
 
-    ###########
+    ########### merge into one json ########
     results = {'results': []}
 
-    for i, j in zip(suites.log_in, suites.suites_in):
-        print("@@@@")
-        k = {**i, **j}
-        print(k)
-        results['results'].append(k)
+    k = {**suites.suites_in, **suites.log_in}
+    for i in captures.capture_in:
+        print(i)
+        for j in k:
+            if i == j:
+                k[j]['expected'] = captures.capture_in[i]['expected']
+                k[j]['actual'] = captures.capture_in[i]['actual']
+    results['results'].append(k)
 
-    ###########
+    ########### write results ###########
 
     with open('data.txt', 'w') as outfile:
         json.dump(results, outfile)
-
-    ##########
-
-    for (item, time_in) in zip(captures.expected, captures.time):
-        DATE_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
-        # a="7/2/10 2:00" #pass the date which is to be formatted to DATE_FORMAT
-        print(item, time_in)
-        # print(time.localtime(time_in))
-        # print(datetime.strptime(str(time_in),DATE_FORMAT))
-        print(time.strptime(time_in, DATE_FORMAT))
-        # print(datetime.strptime(time_in,DATE_FORMAT))
-        # print(type(time.strptime(time_in,DATE_FORMAT)))
-
-    for time_in in results.time:
-        DATE_FORMAT = "%A, %d-%b-%y %H:%M:%S %Z"
-        # a="7/2/10 2:00" #pass the date which is to be formatted to DATE_FORMAT
-        print(item, time_in)
-        # print(time.localtime(time_in))
-        # print(datetime.strptime(str(time_in),DATE_FORMAT))
-        print(time.strptime(time_in, DATE_FORMAT))
-        # print(datetime.strptime(time_in,DATE_FORMAT))
-        # print(type(time.strptime(time_in,DATE_FORMAT)))
-
-    print("----------")
-    print(logs.name)
-    for name, status, expected, output in suites.__dict__.items():
-        print(attr, value)
